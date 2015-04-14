43a1e4931d599ef9d5b494808e07407efaf62cbd
Morten Rasmussen <morten.rasmussen@arm.com>
2015-04-14 16:25:31 +0100

    FROMLIST: arm: Update arch_scale_cpu_capacity() to reflect change to define
    arch_scale_cpu_capacity() is no longer a weak function but a #define
    instead. Include the #define in topology.h.
    
    cc: Russell King <linux@arm.linux.org.uk>
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    
    (am from https://patchwork.kernel.org/patch/6736571)
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    
    Change-Id: I37adeaef311df3900b58bd3dba2ae1c479cf3069

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -42,7 +42,7 @@
  */
 static DEFINE_PER_CPU(unsigned long, cpu_scale);
 
-unsigned long scale_cpu_capacity(struct sched_domain *sd, int cpu)
+unsigned long arm_arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
 {
 #ifdef CONFIG_CPU_FREQ
 	unsigned long max_freq_scale = cpufreq_scale_max_freq_capacity(cpu);
