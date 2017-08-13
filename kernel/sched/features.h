94ab1962bb563da6c3dbabe86c470135740690f4
Morten Rasmussen <morten.rasmussen@arm.com>
2017-08-14 02:24:41 +0900

    FROMLIST: sched: Convert arch_scale_cpu_capacity() from weak function to #define Bring arch_scale_cpu_capacity() in line with the recent change of its arch_scale_freq_capacity() sibling in commit dfbca41f3479 ("sched: Optimize freq invariant accounting") from weak function to #define to allow inlining of the function. While at it, remove the ARCH_CAPACITY sched_feature as well. With the change to #define there isn't a straightforward way to allow runtime switch between an arch implementation and the default implementation of arch_scale_cpu_capacity() using sched_feature. The default was to use the arch-specific implementation, but only the arm architecture provided one. cc: Ingo Molnar <mingo@redhat.com> cc: Peter Zijlstra <peterz@infradead.org> Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com> (am from https://patchwork.kernel.org/patch/6736601) Signed-off-by: Juri Lelli <juri.lelli@arm.com> Change-Id: I0ddec9115ffa6940bbf63b0959a9c73a6ed1acf8 Signed-off-by: DQ <dqstyle@gmail.com>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -36,12 +36,7 @@ SCHED_FEAT(CACHE_HOT_BUDDY, true)
  */
 SCHED_FEAT(WAKEUP_PREEMPTION, true)
 
-/*
- * Use arch dependent cpu capacity functions
- */
-// SCHED_FEAT(ARCH_CAPACITY, true)
 SCHED_FEAT(ARCH_POWER, true)	/* Archpower */
-
 SCHED_FEAT(HRTICK, false)
 SCHED_FEAT(DOUBLE_TICK, false)
 SCHED_FEAT(LB_BIAS, true)
