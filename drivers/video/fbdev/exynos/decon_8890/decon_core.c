e8a95426e6b6cddf31c4fd20536f9bfd7cc1abd4
Lunarixus <Nathanb644@hotmail.co.uk>
2019-07-26 05:17:29 +0100

    decon: Kick devfreq boost when a new frame is committed

--- drivers/video/fbdev/exynos/decon_8890/decon_core.c
+++ drivers/video/fbdev/exynos/decon_8890/decon_core.c
@@ -37,6 +37,8 @@
 #include <linux/of_gpio.h>
 #include <linux/sched.h>
 
+#include <linux/devfreq_boost.h>
+
 #include <media/exynos_mc.h>
 #include <video/mipi_display.h>
 #include <media/v4l2-subdev.h>
@@ -3880,6 +3882,10 @@ windows_config:
 		if (regs->need_update)
 			decon_win_update_rect_reset(decon);
 #endif
+
+		// Boost devfreq before loop
+		devfreq_boost_kick(DEVFREQ_EXYNOS_MIF);
+
 		for (i = 0; i < decon->pdata->max_win; i++) {
 			decon->windows[i]->fbinfo->fix = decon->windows[i]->prev_fix;
 			decon->windows[i]->fbinfo->var = decon->windows[i]->prev_var;
