ecc8f6e899d3225be7ebb6390bce2a0d93a38bb9
ThunderStorms21th <pinakastorm@gmail.com>
2019-10-02 11:10:49 +0200

    Revert "irqchip: Kill off set_irq_flags usage"
    This reverts commit 60a0c52e4d9a35207f992b5901459fbefd599dec.

--- drivers/irqchip/irq-armada-370-xp.c
+++ drivers/irqchip/irq-armada-370-xp.c
@@ -185,6 +185,7 @@ static int armada_370_xp_msi_map(struct irq_domain *domain, unsigned int virq,
 {
 	irq_set_chip_and_handler(virq, &armada_370_xp_msi_irq_chip,
 				 handle_simple_irq);
+	set_irq_flags(virq, IRQF_VALID);
 
 	return 0;
 }
@@ -300,7 +301,7 @@ static int armada_370_xp_mpic_irq_map(struct irq_domain *h,
 		irq_set_chip_and_handler(virq, &armada_370_xp_irq_chip,
 					handle_level_irq);
 	}
-	irq_set_probe(virq);
+	set_irq_flags(virq, IRQF_VALID | IRQF_PROBE);
 
 	return 0;
 }
